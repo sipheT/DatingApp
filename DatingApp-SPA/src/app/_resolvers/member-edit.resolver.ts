cad3bf8ec22927ba19c5482653357d87c9864967
sipheT <1153631@students.wits.ac.za>
2020-05-25 09:56:40 +0200

    basic edit profile

---
+++ DatingApp-SPA/src/app/_resolvers/member-edit.resolver.ts
@@ -0,0 +1,26 @@
+import { User } from '../_models/user';
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRoute, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { UserService } from '../_services/user.service';
+import { Route } from '@angular/compiler/src/core';
+import { AlertifyService } from '../_services/alertify.service';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '../_services/auth.service';
+
+@Injectable()
+export class MemberEditResolver implements Resolve<User>{
+
+    constructor(private userService: UserService, private rooter: Router, private authService: AuthService,
+                private alertify: AlertifyService) {
+    }
+    resolve(route: ActivatedRouteSnapshot): Observable<User> {
+        return this.userService.getUser(this.authService.decodedToken.nameid).pipe(
+            catchError(error => {
+                this.alertify.error('Problem retrieving your data');
+                this.rooter.navigate(['/members']);
+                return of(null);
+            })
+        );
+    }
+}
--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -24,7 +24,9 @@ import { RouterModule } from '@angular/router';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
-
+import { MembersEditComponent } from './members/members-edit/members-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+ 
 
 export function tokengetter(){
    return localStorage.getItem('token');
@@ -48,7 +50,8 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       ListsComponent,
       MessagesComponent,
       MemberCardComponent,
-      MemberDetailComponent
+      MemberDetailComponent,
+      MembersEditComponent
    ],
    imports: [
       BrowserModule,
@@ -72,6 +75,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       AuthService,
       MemberDetailResolver,
       MemberListResolver,
+      MemberEditResolver,
       { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
    ],
    bootstrap: [
